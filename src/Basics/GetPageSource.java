1abc82a4b657ed17e10bc60f5bc0dd8079d4f9a5
260968 - 5FIN <260968 - 5FIN@LAPTOP-TT56SPU4>
2023-11-08 12:19:35 +0530

    Removed auto generated comments in Basics package of 2nd class

--- src/Basics/GetPageSource.java
+++ src/Basics/GetPageSource.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GetPageSource {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.swiggy.com/");
